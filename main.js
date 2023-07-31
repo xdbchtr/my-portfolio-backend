d58c20213b1c4b8013536fd1a1febe1b1bbd22bc
adib.bachtiar <adib.bachtiar@mitra.jasamarga.co.id>
2023-07-31 21:00:19 +0700

    update cors

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const router = require('./routes')
 
 const app = express();
+
+app.use(cors());
 
 const requestLogger = require('./resources/requestLogger');
 
